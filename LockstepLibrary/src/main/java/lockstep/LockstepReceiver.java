12699727887173458e1387342e2e339f77470cff
rzippo <raffaele.zippo@live.it>
2017-02-14 16:06:11 +0100

    SUCCEDONO COSE

--- LockstepLibrary/src/main/java/lockstep/LockstepReceiver.java
+++ LockstepLibrary/src/main/java/lockstep/LockstepReceiver.java
@@ -89,6 +89,7 @@ public class LockstepReceiver implements Runnable
     {
         LOG.debug("1 InputMessage received");
         ExecutionFrameQueue executionFrameQueue = this.executionFrameQueues.get(input.hostID);
+        LOG.debug("Check if is null: " + (input.frame == null ?  "Yes" : "No") );
         FrameACK frameAck = executionFrameQueue.push(input.frame);
         frameAck.setHostID(input.hostID);
         sendACK(frameAck);
@@ -98,6 +99,13 @@ public class LockstepReceiver implements Runnable
     {
         LOG.debug("" + inputs.frames.length + " InputMessages received");
         ExecutionFrameQueue executionFrameQueue = this.executionFrameQueues.get(inputs.hostID);
+        boolean isNotNull = true;
+        for(FrameInput frame : inputs.frames)
+        {
+            if(frame == null)
+                isNotNull = false;
+        }
+        LOG.debug("Check if is null: " + (!isNotNull ?  "Yes" : "No") );
         FrameACK frameAck = executionFrameQueue.push(inputs.frames);
         frameAck.setHostID(inputs.hostID);
         sendACK(frameAck);
