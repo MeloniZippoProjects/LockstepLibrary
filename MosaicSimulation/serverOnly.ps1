e43c3e6d90edf536145eeae499ecd97bcea7d95e
rzippo <raffaele.zippo@live.it>
2017-02-15 17:16:29 +0100

    Powershell scripts

---
+++ MosaicSimulation/serverOnly.ps1
@@ -0,0 +1,9 @@
+﻿$nClients = 10;
+$serverPort = 8000;
+$serverTickrate = 30;
+
+$log = gci -Filter "logs";
+Remove-Item $log -Recurse;
+
+start -FilePath powershell -ArgumentList ("-noExit", "java", "`"-Dlogfile=server`"", "-cp", ".\target\mosaic-1-jar-with-dependencies.jar",
+     "mosaicsimulation.MosaicLockstepServer", "--serverPort=$serverPort", "--nClients=$nClients", "--tickrate=$serverTickrate");
---
+++ MosaicSimulation/simulation.ps1
@@ -0,0 +1,19 @@
+﻿$nClients = 10;
+$serverAddress = "localhost";
+$serverPort = 8000;
+$clientFramerate = 60;
+$clientTickrate = 30;
+$serverTickrate = 30;
+$fillTimeout = 500;
+$fillSize = 10;
+
+$log = gci -Filter "logs";
+Remove-Item $log -Recurse;
+
+start -FilePath powershell -ArgumentList ("-noExit", "java", "`"-Dlogfile=server`"", "-cp", ".\target\mosaic-1-jar-with-dependencies.jar",
+     "mosaicsimulation.MosaicLockstepServer", "--serverPort=$serverPort", "--nClients=$nClients", "--tickrate=$serverTickrate");
+for($i = 0; $i -lt $nClients; $i++)
+{
+    start -FilePath powershell -ArgumentList ("-noExit", "java", "-Dlogfile=client_$i", "-cp", ".\target\mosaic-1-jar-with-dependencies.jar", "mosaicsimulation.MosaicSimulation",
+     "--serverIPAddress=$serverAddress", "--serverTCPPort=$serverPort", "--framerate=$clientFramerate","--tickrate=$clientTickrate", "--fillTimeout=$fillTimeout", "--fillSize=$fillSize") 
+}
